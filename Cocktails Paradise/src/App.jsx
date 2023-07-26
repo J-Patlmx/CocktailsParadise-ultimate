7181f3b455baeda383801bee70f75c291261c225
J-Patlmx <lamoureux.jp61@gmail.com>
2023-07-26 17:34:20 +0200

    ADD meta data try to ADD Cookie selector

--- Cocktails Paradise/src/App.jsx
+++ Cocktails Paradise/src/App.jsx
@@ -1,22 +1,35 @@
-import { useState } from 'react'
+
 import Cocktail from './components/Cocktail'
 import './styles/styles.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
 
       <h1>Cocktails Paradise</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+
         <div>
           <Cocktail />
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+         {/* texte a remplir Bienvenue sur Cocktails Paradise, ici, on peut apprendre a réalisé des cocktails grâce à des petits
+                tutos vidéo,<br /> alors on n'hésite pas à tips le barman.
+                L'alcool se consomme avec modération. Même s'il y a des fruits ! <br />
+
+
+                Une personne alcoolisée ne veut pas dire consentement automatique alors on respecte le NON.<br />
+
+                L'alcoolisme est une maladie alors on ne rigole pas avec ça et on reste tous bien veillant les uns en
+                vers les autres.<br />
+
+                Nous ne sommes pas partenaires, mais ça va dans le bon sens.
+                <a id="lienCapuchon" href="https://drink-watch.com/produit/">DRINK WATCH</a> est un outil de prévention
+                et de dissuasion.<br />
+
+
+                Ne laissez jamais votre verre sans surveillance. <br />
+                Bonne dégustation et comme on dit chez moi "yer'mat". */}
         </p>
         </div>
       </div>
