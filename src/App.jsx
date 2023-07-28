85d7399c9ec703f3bc023fe796623b6f998b3d8a
J-Patlmx <lamoureux.jp61@gmail.com>
2023-07-28 23:23:15 +0200

    resolution erreur ' avec &#39;

--- src/App.jsx
+++ src/App.jsx
@@ -11,24 +11,14 @@ function App() {
 
         <div>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, recusandae perferendis reiciendis cumque officiis itaque sequi neque excepturi! Illum nisi reprehenderit, sit veniam esse quis. Odio ut amet quod dolore?
-          Cum, odio sit. Minus, ipsum repudiandae! Odio illum repellendus consequatur mollitia! Ullam, libero voluptatum? Ipsum sapiente porro atque quaerat incidunt consectetur accusamus ad natus? Eveniet earum nostrum deserunt repudiandae nemo.
-          Rerum nihil alias ipsam excepturi tempore eos dolorem provident fuga sed omnis quia neque sapiente distinctio architecto officia atque nesciunt, dolor, autem magni laboriosam eligendi? Quasi temporibus deserunt cum accusantium?
-          Amet minima dolor fuga maiores illo quibusdam voluptatum id rem! Quisquam error quia aperiam esse sequi, quod id cumque totam vel temporibus fuga distinctio quaerat repudiandae placeat est voluptates maiores!
-          Soluta voluptas amet, maiores dolorum minima, qui corporis excepturi eaque natus deserunt iure reiciendis cum voluptate accusamus placeat sunt libero fugit! Provident voluptas accusantium dolor aliquam magnam impedit error perferendis!
-          Voluptates exercitationem facere fugiat harum perferendis ducimus excepturi odio aut, vel doloremque alias expedita asperiores modi fugit dolorem quis possimus nisi adipisci quos quasi quia! Deserunt obcaecati error dolorem facere!
-          Eaque, et. Eos perferendis voluptates eveniet, molestias animi consequatur necessitatibus repellat quas deleniti alias magni velit consequuntur voluptas asperiores quibusdam consectetur accusantium voluptatibus nam commodi quisquam! Iure commodi sequi inventore.
-          Numquam suscipit earum mollitia nesciunt, animi eos ipsum cupiditate dolorum dolore voluptas ex ut illo. Nesciunt hic, repellendus quas, fugiat, libero quae animi tempore vero dicta ducimus voluptatum? Voluptatibus, dolor?
-          Nulla, minus ut consequatur, illum incidunt asperiores obcaecati minima nemo voluptatum commodi accusamus quibusdam! Suscipit consequatur sunt adipisci, nobis dolores est, maiores ratione a sapiente fuga provident sequi pariatur officia.
-          Ipsum quia est inventore eveniet officia cumque placeat esse, magni impedit sunt iusto velit molestias voluptate, odio recusandae? Tenetur nesciunt iure quam iusto, libero minima nostrum laudantium id corrupti eaque.
-         {/* texte a remplir Bienvenue sur Cocktails Paradise, ici, on peut apprendre a réalisé des cocktails grâce à des petits
-                tutos vidéo,<br /> alors on n"hésite pas à tips le barman.
-                L"alcool se consomme avec modération. Même s"il y a des fruits ! <br />
+ Bienvenue sur Cocktails Paradise, ici, on peut apprendre a réalisé des cocktails grâce à des petits
+                tutos vidéo,<br /> alors on n&#39;hésite pas à tips le barman.
+                L&#39;alcool se consomme avec modération. Même sil y a des fruits ! <br />
 
 
                 Une personne alcoolisée ne veut pas dire consentement automatique alors on respecte le NON.<br />
 
-                L"alcoolisme est une maladie alors on ne rigole pas avec ça et on reste tous bien veillant les uns en
+                L&#39;alcoolisme est une maladie alors on ne rigole pas avec ça et on reste tous bien veillant les uns en
                 vers les autres.<br />
 
                 Nous ne sommes pas partenaires, mais ça va dans le bon sens.
@@ -37,7 +27,7 @@ function App() {
 
 
                 Ne laissez jamais votre verre sans surveillance. <br />
-                Bonne dégustation et comme on dit chez moi "yer"mat". */}
+                Bonne dégustation et comme on dit chez moi <q>yer&#39;mat</q>.
         </p>
         </div>
       </div>
