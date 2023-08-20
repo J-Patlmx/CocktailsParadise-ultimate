725ba2ab3053c2d487c80a562b0829498e6b68bc
J-Patlmx <lamoureux.jp61@gmail.com>
2023-08-20 13:53:42 +0200

    REFACT SCSS 1st

--- src/pages/Nos_Cocktails.jsx
+++ src/pages/Nos_Cocktails.jsx
@@ -1,26 +1,26 @@
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 
 function Nos_Cocktails() {
   return (<>
-      <h1 className="Title-nosCocktails">Nos Cocktails</h1>
-  <div className="Cocktail-link-general">
-    <nav className="Cocktail-link">
-         {/* Utiliser l'attribut "to" pour spécifier l'URL de destination */}
-<div className="Cocktails-choise">
-            <NavLink className="App-Cocktail-link" to={"/cocktailSA"}>Nos cocktails sans alcools</NavLink>
-            <img src="https://zupimages.net/up/23/30/hod4.jpg" alt="un cocktail" />          
-        
-            </div>
+    <h1 className="Title-nosCocktails">Nos Cocktails</h1>
+    <div className="Cocktail-link-general">
+      <nav className="Cocktail-link">
+        {/* Utiliser l'attribut "to" pour spécifier l'URL de destination */}
+        <div className="Cocktails-choise">
+          <NavLink className="App-Cocktail-link" to={"/cocktailSA"}>Nos cocktails sans alcools</NavLink>
+          <img src="https://zupimages.net/up/23/30/hod4.jpg" alt="un cocktail" />
 
-            <div className="Cocktails-choise">
-            <NavLink className="App-Cocktail-link" to={"/cocktailAA"}>Nos cocktails avec alcools</NavLink>
-            <img src="https://zupimages.net/up/23/30/cgpj.jpg" alt="un cocktail" />          
-                    
+        </div>
 
-            </div>
-          </nav>
- </div></> )
+        <div className="Cocktails-choise">
+          <NavLink className="App-Cocktail-link" to={"/cocktailAA"}>Nos cocktails avec alcools</NavLink>
+          <img src="https://zupimages.net/up/23/30/cgpj.jpg" alt="un cocktail" />
+
+
+        </div>
+      </nav>
+    </div></>)
 }
 
 export default Nos_Cocktails
