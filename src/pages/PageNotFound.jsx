931debd047238450888a027ca50fbdf26c42743a
J-Patlmx <lamoureux.jp61@gmail.com>
2023-08-21 11:55:50 +0200

    WIP 1st REFACT

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,8 @@
 function PageNotFound() {
   return (
     <div className="Page-not-found">
-    <h1>404</h1>
-   <p> La page a été bue par le Barman.</p>
-    
+        <h1>404</h1>
+        <p> La page a été bue par le Barman.</p>
     </div>
   )
 }
