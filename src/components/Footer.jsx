bbf4b141faf13e48ac1b2df3a2069f57dee8bd72
J-Patlmx <lamoureux.jp61@gmail.com>
2023-08-25 22:38:31 +0200

    WIP ADD social network

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import { Link } from "react-router-dom"
+import facebook from "../assets/facebook.png"
+import instagram from "../assets/instagram.png"
+
 function Footer() {
   return (
+    <>
     <Link className="Footerlink footer-Texte" to={"https://j-patlmx.github.io/businessCard-main/"} >©Cocktails Paradise par DevDread</Link>
+    <Link  to={"https://j-patlmx.github.io/businessCard-main/"}><img src={facebook}/></Link>
+    <img src={instagram} />
+    </>
   )
 }
 
