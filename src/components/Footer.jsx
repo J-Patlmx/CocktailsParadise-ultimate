c1ad4f9cf0ae1214bc25db9077c288801050d668
J-Patlmx <lamoureux.jp61@gmail.com>
2023-08-26 20:24:35 +0200

    ADD className WIP footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,11 @@ import instagram from "../assets/instagram.png"
 
 function Footer() {
   return (
-    <>
-    <Link className="Footerlink footer-Texte" to={"https://j-patlmx.github.io/businessCard-main/"} >©Cocktails Paradise par DevDread</Link>
-    <Link  to={"https://www.facebook.com/officiel.Cocktails.Paradise/"}><img src={facebook}/></Link>
-    <Link  to={"https://www.instagram.com/cocktails_paradise/"}><img src={instagram}/></Link>
-    </>
+    <div className="Footerlink">
+    <Link className="footer-Texte" to={"https://j-patlmx.github.io/businessCard-main/"} >©Cocktails Paradise par DevDread</Link>
+    <Link className="footer-Texte-rs" to={"https://www.facebook.com/officiel.Cocktails.Paradise/"}><img src={facebook}/></Link>
+    <Link className="footer-Texte-rs" to={"https://www.instagram.com/cocktails_paradise/"}><img src={instagram}/></Link>
+    </div>
   )
 }
 
