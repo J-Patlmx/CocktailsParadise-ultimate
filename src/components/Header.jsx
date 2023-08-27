6a26179b6ab149ee54a9f0905e9968732426dfed
J-Patlmx <lamoureux.jp61@gmail.com>
2023-08-27 19:24:08 +0200

    WIP hamburger Menu MQ

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,35 @@
-import {NavLink} from "react-router-dom"
-import logo from "../assets/logo2.png"
-import MusicPlayer  from "./audioplayer"
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo2.png";
+import MusicPlayer from "./audioplayer";
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-<div className="Header">
-<img src={logo} className="App-logo" alt="logo" />
-          <MusicPlayer  />
-<nav className="Header-link">
-         {/* Utiliser l'attribut "to" pour spécifier l'URL de destination */}
-            <NavLink className="App-header-link" to={"/home"} >Accueil</NavLink>
-            <NavLink className="App-header-link" to={"/cocktail"}>Cocktails </NavLink>
-            <NavLink className="App-header-link" to={"/Contact"}>Contact </NavLink>
-          </nav>
-          {/* TODO: revoir le positionnement */}
-  
+    <div className={`Header ${isMenuOpen ? "open" : ""}`}>
+      <img src={logo} className="App-logo" alt="logo" />
+      <MusicPlayer />
+      <button className="menu-button" onClick={toggleMenu}>
+        ☰
+      </button>
+      <nav className={`Header-link ${isMenuOpen ? "open" : ""}`}>
+        <NavLink className="App-header-link" to={"/home"}>
+          Accueil
+        </NavLink>
+        <NavLink className="App-header-link" to={"/cocktail"}>
+          Cocktails
+        </NavLink>
+        <NavLink className="App-header-link" to={"/Contact"}>
+          Contact
+        </NavLink>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
