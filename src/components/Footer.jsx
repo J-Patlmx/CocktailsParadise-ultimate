51d8bf3ec195be5e51360f1fb328ef433e8fcddb
J-Patlmx <lamoureux.jp61@gmail.com>
2024-08-22 17:53:38 +0200

    add nex dev dread url

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import instagram from "../assets/instagram.png"
 function Footer() {
   return (
     <div className="Footerlink">
-    <Link className="footer-Texte" to={"https://j-patlmx.github.io/businessCard-main/"} >©Cocktails Paradise par DevDread</Link>
+    <Link className="footer-Texte" to={"https://dev-dread.vercel.app"} >©Cocktails Paradise par DevDread</Link>
     <Link className="footer-Texte-rs" to={"https://www.facebook.com/officiel.Cocktails.Paradise/"}><img className="Img-Footer-Rs" src={facebook} alt="lettre f de facebook"/> </Link>
     <Link className="footer-Texte-rs" to={"https://www.instagram.com/cocktails_paradise/"}><img className="Img-Footer-Rs" src={instagram} alt="appareil photo d'instagram "/> </Link>
     </div>
